e8022800fd0a8f84d29955c57acf9a5c674703f8
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-05-30 09:38:58 +0200

    test

---
+++ src/test/java/FizzBuzzProcessorTest.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzProcessorTest {
+
+    @Test
+    public void FizzBuzzNormalNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assert.assertEquals("1", fb.convert(1));
+        Assert.assertEquals("2", fb.convert(2));
+    }
+
+    @Test
+    public void FizzBuzzThreeNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assert.assertEquals("Fizz", fb.convert(3));
+    }
+
+    @Test
+    public void FizzBuzzFiveNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assert.assertEquals("Buzz", fb.convert(5));
+    }
+
+    @Test
+    public void FizzBuzzThreeAndFiveNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assert.assertEquals("Buzz", fb.convert(5));
+    }
+}
